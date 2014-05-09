8b7008e3c4180d4592a072e7b4af1515a1a53856
rayng <rayng83@gmail.com>
2014-05-09 13:24:23 -0400

    I have now incorporated glob into the program to get the data files from the relevant directory.

--- genphaseplots.py
+++ genphaseplots.py
@@ -2,6 +2,17 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
+
 
 print "test"
+tf=0.6595
+
+#use glob to list out files that we want in this case:
+# This is the main input to the program
+dir='/home/ngray/ORCA/GPE/SRC/free-exp-data/nr1/*dat'
+files=glob.glob(dir)
+
+    
+
 

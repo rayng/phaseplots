23123238429f3094fc90ded84cfac271e71a96bd
rayng <rayng83@gmail.com>
2014-05-09 14:24:51 -0400

    I have now added the ability to create color density plots using the plt.scatter and choosing the right pixel size. A color bar has been added and the figure namings are now automated by adding '.png' extension to the source file. Also note that this program take two arugments:
    1. tf
    2. location of data files

--- genphaseplots.py
+++ genphaseplots.py
@@ -4,15 +4,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-
-print "test"
-tf=0.6595
-
 #use glob to list out files that we want in this case:
 # This is the main input to the program
-dir='/home/ngray/ORCA/GPE/SRC/free-exp-data/nr1/*dat'
+
+
+tf=0.6595
+dir='/home/ngray/ORCA/GPE/SRC/free-exp-data/nr1/*'+str(tf)+'*.dat'
 files=glob.glob(dir)
 
+#iterate through files
+for f in files:
+    # parse file name using "/" delimiter
+    fsplit=f.split("/")
+    # extract last element which is the file name
+    fn=fsplit[len(fsplit)-1]
+    # load text
+    intpattern=np.loadtxt(f)
+    arr=intpattern.transpose()
+    x,y,z = arr[0], arr[1], arr[2]
+    plt.scatter(x, y, c=z, s=50, edgecolor='')
+    plt.xlim(min(x), max(x))
+    plt.ylim(min(y), max(y))
+    plt.ylabel(r"$z$")
+    plt.xlabel(r"$x$")    
+    plt.colorbar()
+    plt.savefig('./figs/'+fn+'.png')
+    plt.clf()
     
-
-
